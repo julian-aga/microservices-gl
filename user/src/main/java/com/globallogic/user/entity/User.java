d4bfe7c85cc9e840e897ee65ecc258b6d379f3ca
unknown <julian.aga@globallogic.com>
2022-03-31 21:38:36 -0300

    fixed import sql

--- user/src/main/java/com/globallogic/user/entity/User.java
+++ user/src/main/java/com/globallogic/user/entity/User.java
@@ -5,25 +5,18 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.FetchType;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.OneToMany;
-import javax.persistence.PrePersist;
-import javax.persistence.PreUpdate;
+import javax.persistence.*;
 import java.io.Serializable;
 import java.time.LocalDate;
 import java.util.HashSet;
 import java.util.Set;
 
-@Data
 @Entity
+@Data
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
+@Table(name="users")
 public class User implements Serializable {
     private static final long serialVersionUID= 1L;
     @Id
